efff5c39532c7b970118a850d962b859e320c5ff
Moneshsoni <sonimonesh115@gmail.com>
2023-10-09 17:03:39 +0530

    add the read editor in active admin

--- app/admin/blogs.rb
+++ app/admin/blogs.rb
@@ -7,6 +7,40 @@ ActiveAdmin.register Blog do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+  # controller do 
+  #   def update
+  #     debugger
+  #   end
+  # end
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :description do |object|
+      object.description.html_safe
+    end
+    actions
+  end
+  
+  show do |t|
+
+    attributes_table do
+      # if t.images.attached?
+      #   t.images.each do |img|
+      #     span do
+      #       image_tag img.variant(resize_to_limit: [100, 100])
+      #     end
+      #   end
+      # end
+      row :title
+      row :created_at
+      row :updated_at
+      row :description do |object|
+        object.description.html_safe
+      end
+    end
+  end
 
   form do |f|
     f.inputs do
